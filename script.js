6d3d93e2e0c58076cd9ef9cb28b7b43d15e92e61
Gurusamy-zoho <gurusamysivasankar591@gmail.com>
2025-03-25 19:51:38 +0530

    feat: added swal fire alert and position:center

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
-// document.getElementById('getWeather').addEventListener('click',()=>{
-//     let city = document.getElementById('city').value;
-//     if(city===""){
-//         alert("Enter city name");
-//         return;
-//     }
-
-//     let apiKey = '1348bfa57632645b961f854a16ef9a20';
-//     let xhr = new XMLHttpRequest();
-
-//         xhr.open('GET',`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,true);
-//         xhr.onload = function () {
-//             if (xhr.readyState === 4 && xhr.status === 200) {
-//                 let data = JSON.parse(xhr.responseText);
-                
-//                 let weatherDetails = document.getElementById('weatherDetails');
-//                 let weatherInfo = `
-//                     <h2>${data.name}, ${data.sys.country}</h2>
-//                     <p><strong>Temperature:</strong> ${data.main.temp}°C</p>
-//                     <p><strong>Weather:</strong> ${data.weather[0].description}</p>
-//                     <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
-//                     <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
-//                 `;
-                
-//                 weatherDetails.innerHTML = weatherInfo;
-//                 weatherDetails.style.display = 'block';
-//             }
-
-//             else if(xhr.readyState===404){
-//                 alert("city not found")
-//                 return;
-//             }
-//         };
-//         xhr.send();
-// });
 
 document.getElementById('getWeather').addEventListener('click', () => {
     let city = document.getElementById('city').value;
     if (city === "") {
-        alert("Enter city name");
-        return;
+        Swal.fire({
+            title: 'Error',
+            html: `Invalid Input`,
+            icon: 'error',
+            confirmButtonText: 'Ok',
+            timer: 3000,
+            position:'center',
+            heightAuto: false,
+            showClass: {
+              popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__fadeOutUp'
+            }
+          });
+
+          return
     }
 
     let apiKey = '1348bfa57632645b961f854a16ef9a20';
@@ -50,7 +30,7 @@ document.getElementById('getWeather').addEventListener('click', () => {
             let data = JSON.parse(xhr.responseText);
             
             let condition = data.weather[0].main.toLowerCase(); 
-            
+
             let weatherIcons = {
                 "clear": "☀️",       
                 "clouds": "⛅",       
